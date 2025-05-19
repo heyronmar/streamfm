b0ed4d33978db507d7b4248e531de7dca1debeaf
heyronmar <weroarltd@gmail.com>
2025-05-19 20:02:40 +1200

    initial commit

---
+++ src/app/constants/station.ts
@@ -0,0 +1,40 @@
+// TypeScript interface for a radio station entry
+export interface Station {
+    changeuuid: string;
+    stationuuid: string;
+    serveruuid: string;
+    name: string;
+    url: string;
+    url_resolved: string;
+    homepage: string;
+    favicon: string;
+    tags: string;
+    country: string;
+    countrycode: string;
+    iso_3166_2: string;
+    state: string;
+    language: string;
+    languagecodes: string;
+    votes: number;
+    lastchangetime: string;
+    lastchangetime_iso8601: string;
+    codec: string;
+    bitrate: number;
+    hls: number;
+    lastcheckok: number;
+    lastchecktime: string;
+    lastchecktime_iso8601: string;
+    lastcheckoktime: string;
+    lastcheckoktime_iso8601: string;
+    lastlocalchecktime: string;
+    lastlocalchecktime_iso8601: string;
+    clicktimestamp: string;
+    clicktimestamp_iso8601: string;
+    clickcount: number;
+    clicktrend: number;
+    ssl_error: number;
+    geo_lat: number;
+    geo_long: number;
+    geo_distance: number | null;
+    has_extended_info: boolean;
+}
